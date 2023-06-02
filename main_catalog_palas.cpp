71759490b1d0e4fa9d01cde355b18d7677bc494c
Aceeeeeeeeeeeeeee <117962542+Aceeeeeeeeeeeeeee@users.noreply.github.com>
2023-06-01 22:47:07 -0600

    Add files via upload

--- main_catalog_palas.cpp
+++ main_catalog_palas.cpp
@@ -14,58 +14,95 @@
 //Bibliotecas
 
 #include <iostream>
-#include <string>
 
 using namespace std;
 
+#include "catalogo.h"
 #include "pala.h"
 
 
-int main(){
+void  menu() {
 
-    int temp_id = 1;
+    cout << "\nMENU:\n\n";
+    cout << "1. Agrega una pala de alta gama\n";
+    cout << "2. Agrega una pala de baja gama\n";
+    cout << "3. Muesta todas las palas\n";
+    cout << "4. Muesta ciertas palas de acuerdo" <<
+    " a su gama\n";
+    cout << "5. Muesta ciertas palas de acuerdo" <<
+    " a un costo maximo\n";
+    cout << "6. Calcula el costo de todas las" <<
+    " palas\n";
+    cout << "7. Calcula el costo de todas las" <<
+    " palas de acuerdo a su gama\n";
+    cout << "8. Salir\n\n";
+
+}
+
+
+int main() {
+
+    Catalogo catalogo;
+    catalogo.crea_ejemplos();
+
+    int opcion = 0;
+
+    string temp_nombre, temp_gama;
     
-    string temp_nombre, temp_tipo, temp_material,
-    temp_forma;
-    
-    float temp_peso, temp_durabilidad, temp_calidad,
+    float temp_peso, temp_calidad, temp_durabilidad,
     temp_desvalance;
 
+    while(opcion < 8 && opcion > -1) {
 
-    cout << "\nIngresa el nombre de la pala: ";
-    cin >> temp_nombre;
-    cout << "\nIngresa el peso gramos: ";
-    cin >> temp_peso;
-    cout << "\nIngresa el tipo de pala: ";
-    cin >> temp_tipo;
-    cout << "\nIngresa el material de la pala: ";
-    cin >> temp_material;
-    cout << "\nIngresa el forma: ";
-    cin >> temp_forma;
-    cout << "\nIngresa la durabilidad de la pala: ";
-    cin >> temp_durabilidad;
-    cout << "\nIngresa la calidad de la pala: ";
-    cin >> temp_calidad;
+        cout << "¿Que desea realizar? ";
+        cin >> opcion;
 
-    AltaGama pala_nox(temp_id, temp_nombre, temp_peso,
-    temp_tipo, temp_material, temp_forma,
-    temp_durabilidad, temp_calidad);
+        switch(opcion) {
 
-    cout << pala_nox.to_string();
+            case 1:
 
+                cout << "\nIngresa el nombre de la pala: ";
+                cin >> temp_nombre;
+                cout << "\nIngresa el peso gramos: ";
+                cin >> temp_peso;
+                cout << "\nIngresa la calidad de la pala: ";
+                cin >> temp_calidad;
+                cout << "\nIngresa la durabilidad de la pala: ";
+                cin >> temp_durabilidad;
 
-    cout << "\nIngresa el nombre de la pala: ";
-    cin >> temp_nombre;
-    cout << "\nIngresa el peso gramos: ";
-    cin >> temp_peso;
-    cout << "\nIngresa el tipo de pala: ";
-    cin >> temp_tipo;
-    cout << "\nIngresa el material de la pala: ";
-    cin >> temp_material;
-    cout << "\nIngresa la calidad de la pala: ";
-    cin >> temp_calidad;
-    cout << "\nIngresa el desvalance de la pala: ";
-    cin >> temp_desvalance;
+                catalogo.agrega_gama_alta(temp_nombre,
+                temp_peso, temp_calidad, temp_durabilidad);
+
+                break;
+
+            case 2:
+
+                cout << "\nIngresa el nombre de la pala: ";
+                cin >> temp_nombre;
+                cout << "\nIngresa el peso gramos: ";
+                cin >> temp_peso;
+                cout << "\nIngresa la calidad de la pala: ";
+                cin >> temp_calidad;
+                cout << "\nIngresa el desvalance de la pala: ";
+                cin >> temp_desvalance;
+
+                catalogo.agrega_gama_alta(temp_nombre,
+                temp_peso, temp_calidad, temp_desvalance);
+
+                break;
+            
+            case 3:
+
+                catalogo.muestra_palas();
+
+                break;
+
+            case 4:
+
+                cout >> "Ingresa A";
+    
+        }
+    }
 
     BajaGama pala_wilson(temp_id, temp_nombre, temp_peso,
     temp_tipo, temp_material, temp_calidad, temp_desvalance);
